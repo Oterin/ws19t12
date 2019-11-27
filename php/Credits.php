e096e4780373f0fd70de5cd6cb9263c9906b45ed
Eneko Cuesta <38286159+enekocuesta@users.noreply.github.com>
2019-11-27 21:55:00 +0100

    kreditu aldaketa

--- php/Credits.php
+++ php/Credits.php
@@ -8,7 +8,17 @@
   <section class="main" id="s1">
     <div>
 
-		<h1>Mikel Echeverria</h1>
+		<h1>Aitor Otero</h1>
+		<h4>Espezialitatea: Softwarea</h4>
+		<img src="../images/Mikel.jpg" height="100px">
+		<h5>Udalerria: Gallarta</h5>
+		<br><br>
+		<h1>Eneko Cuesta</h1>
+		<h4>Espezialitatea: Softwarea</h4>
+		<img src="../images/Ibon.jpg" height="100px">
+		<h5>Udalerria: Hernani</h5>
+	    
+	 	<h1>Mikel Echeverria</h1>
 		<h4>Espezialitatea: Softwarea</h4>
 		<img src="../images/Mikel.jpg" height="100px">
 		<h5>Udalerria: Urnieta</h5>
